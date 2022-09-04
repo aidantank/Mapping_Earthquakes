10c1522c08cf4da334d033ad13907c013e95a959
Aidan Tank <aidan.tank@gmail.com>
2022-09-04 15:37:45 -0400

    Updating logic.js

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,12 +2,34 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([39.8283, -98.5795], 5);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    // SFO
+    [33.9416, -118.4085],
+    // AUS
+    [30.1975, -97.6664],
+    // DTW
+    [42.2162, -83.3554],
+    // YYZ
+    [43.6777, -79.6248],
+    // JFK
+    [40.6413, -73.7781]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    weight: 4,
+    opacity: 0.5,
+    color: "blue",
+    dashArray: "10"
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style_id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    style_id: "streets-v11",
+    style_id: "light-v10",
     maxZoom: 18,
     accessToken: API_KEY
 });
